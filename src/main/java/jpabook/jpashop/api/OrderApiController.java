bdc2d89e5dd7e882954c97e584a199bc7d3549a3
xxxelppa <xxxelppa@gmail.com>
2020-10-03 14:46:37 +0900

    Day 04 :: API 개발 고급 - 컬렉션 조회 최적화  - 주문 조회 v4 : JPA 에서 DTO 직접 조회  - 주문 조회 v5 : JPA 에서 DTO 직접 조회 (컬렉션 조회 최적화)  - 주문 조회 v6 : JPA 에서 DTO 직접 조회 (플랫 데이터 최적화)  - API 개발 고급 정리

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,10 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +20,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
     
     @GetMapping("/api/v1/orders")
     public List<Order> orderV1() {
@@ -40,7 +47,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
     
@@ -50,7 +57,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllWithItem();
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
     
@@ -70,9 +77,135 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
+    
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> orderV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+    
+    
+    
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+        
+        /*
+            =============================================================================
+            == EXECUTE QUERY
+            =============================================================================
+            select
+                order0_.order_id as col_0_0_,
+                member1_.name as col_1_0_,
+                order0_.order_date as col_2_0_,
+                order0_.status as col_3_0_,
+                delivery2_.city as col_4_0_,
+                delivery2_.street as col_4_1_,
+                delivery2_.zipcode as col_4_2_
+            from
+                orders order0_
+            inner join
+                member member1_
+                    on order0_.member_id=member1_.member_id
+            inner join
+                delivery delivery2_
+                    on order0_.delivery_id=delivery2_.delivery_id
+            =============================================================================
+            select
+                orderitem0_.order_id as col_0_0_,
+                item1_.name as col_1_0_,
+                orderitem0_.order_price as col_2_0_,
+                orderitem0_.count as col_3_0_
+            from
+                order_item orderitem0_
+            inner join
+                item item1_
+                    on orderitem0_.item_id=item1_.item_id
+            where
+                orderitem0_.order_id in (
+                    ? , ?
+                )
+            =============================================================================
+         */
+    }
+    
+    @GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> orderV6() {
+        return orderQueryRepository.findAllByDto_flat();
+        
+        /*
+            =============================================================================
+            == EXECUTE QUERY
+            =============================================================================
+            select
+                order0_.order_id as col_0_0_,
+                member1_.name as col_1_0_,
+                order0_.order_date as col_2_0_,
+                order0_.status as col_3_0_,
+                delivery2_.city as col_4_0_,
+                delivery2_.street as col_4_1_,
+                delivery2_.zipcode as col_4_2_,
+                item4_.name as col_5_0_,
+                orderitems3_.order_price as col_6_0_,
+                orderitems3_.count as col_7_0_
+            from
+                orders order0_
+            inner join
+                member member1_
+                    on order0_.member_id=member1_.member_id
+            inner join
+                delivery delivery2_
+                    on order0_.delivery_id=delivery2_.delivery_id
+            inner join
+                order_item orderitems3_
+                    on order0_.order_id=orderitems3_.order_id
+            inner join
+                item item4_
+                    on orderitems3_.item_id=item4_.item_id
+            =============================================================================
+            
+         */
+    }
+    
+    
+    @GetMapping("/api/v6.1/orders")
+    public List<OrderQueryDto> orderV6_mapping_OrderQueryDto() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+    
+        return flats.stream()
+                .collect(
+                        groupingBy(
+                                o -> new OrderQueryDto(
+                                        o.getOrderId()
+                                        , o.getName()
+                                        , o.getOrderDate()
+                                        , o.getOrderStatus()
+                                        , o.getAddress()
+                                ),
+                                mapping(o -> new OrderItemQueryDto(
+                                        o.getOrderId()
+                                        , o.getItemName()
+                                        , o.getOrderPrice()
+                                        , o.getCount())
+                                        , toList()
+                                )
+                        )
+                )
+                .entrySet().stream()
+                    .map(
+                            e -> new OrderQueryDto(
+                                e.getKey().getOrderId()
+                                , e.getKey().getName()
+                                , e.getKey().getOrderDate()
+                                , e.getKey().getOrderStatus()
+                                , e.getKey().getAddress()
+                                , e.getValue()
+                            )
+                    ).collect(toList());
+    }
+    
     
     /*
      * DTO 를 만들었지만, 그 안에 Entity 를 그대로 사용하는 부분이 있음.
@@ -122,7 +255,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
             orderItems = order.getOrderItems().stream()
                     .map(OrderItemDto::new)
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
     
